db893fb9287d6d6da9eb42a9146ec99306b46bf1
Andy Gomez parra <agomez@Andys-Laptop.local>
2025-09-02 23:57:11 -0400

    remove shadow effects

--- src/home-manager/hyprland.nix
+++ src/home-manager/hyprland.nix
@@ -54,11 +54,6 @@ in
         };
       };
 
-      drop_shadow = true;
-      shadow_range = 4;
-      shadow_render_power = 3;
-      "col.shadow" = "rgba(1a1a1aee)";
-
       animations = {
         enabled = true;
         bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
