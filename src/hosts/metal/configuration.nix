7cd8296dbe58a9879aa0c6c45ee0e1a211f4c046
Andy Gomez parra <agomez@Andys-Laptop.local>
2025-09-03 21:32:09 -0400

    update metal config

--- src/hosts/metal/configuration.nix
+++ src/hosts/metal/configuration.nix
@@ -4,41 +4,10 @@
       ./hardware-configuration.nix
   ]; 
 
-  # Basic system settings 
-  boot.loader.systemd-boot.enable = true; # Enable systemd-boot
-  boot.loader.efi.canTouchEfiVariables = true; # Required for EFI systems
-  time.timeZone = "UTC"; 
-
-  # User configuration 
-  users.users.andy = { 
-    isNormalUser = true; 
-    extraGroups = [ "wheel" "networkmanager" ]; 
-  }; 
-
   hardware.nvidia = {
     modesetting.enable = true;
     prime = {
       offload.enable = true;
     };
   };
-  services.xserver.videoDrivers = [ "nvidia" ];
-  # Enable necessary services 
-  services.xserver.enable = true; 
-  services.xserver.displayManager.startx.enable = true; 
-  services.xserver.windowManager.hyprland.enable = true; 
-  services.wayland.enable = true; 
-  services.waybar.enable = true; 
-
-  # System packages 
-  environment.systemPackages = with pkgs; [ 
-    hyprland 
-    hyprpaper 
-    wayland 
-    waybar 
-    vim 
-    git
-    ghostscript 
-    neovim
-    kitty
-  ]; 
 }
